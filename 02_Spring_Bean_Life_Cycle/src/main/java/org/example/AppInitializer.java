0a3511d140591dbcbcb9deec24241ad33907150e
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-19 09:57:49 +0530

    edit

--- 02_Spring_Bean_Life_Cycle/src/main/java/org/example/AppInitializer.java
+++ 02_Spring_Bean_Life_Cycle/src/main/java/org/example/AppInitializer.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.bean.TestBean1;
+import org.example.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppInitializer {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+        TestBean1 testBean1 = context.getBean(TestBean1.class);
+        System.out.println(testBean1);
+        context.registerShutdownHook();
+
 
     }
 }
--- 02_Spring_Bean_Life_Cycle/src/main/java/org/example/config/AppConfig.java
+++ 02_Spring_Bean_Life_Cycle/src/main/java/org/example/config/AppConfig.java
@@ -1,5 +1,12 @@
 package org.example.config;
 
+import org.example.bean.TestBean1;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackageClasses = TestBean1.class)
 public class AppConfig {
 
 }
