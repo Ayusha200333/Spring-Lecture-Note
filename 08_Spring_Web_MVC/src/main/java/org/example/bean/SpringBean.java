851f4099e8d9825c3125829c505e3d4962bf69bd
Ayusha200333 <ayushawijerathna@gmail.com>
2025-06-26 14:19:20 +0530

    add spring web mvc

---
+++ 08_Spring_Web_MVC/src/main/java/org/example/bean/SpringBean.java
@@ -0,0 +1,39 @@
+package org.example.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean  implements DisposableBean , InitializingBean , ApplicationContextAware  , BeanNameAware  , BeanFactoryAware {
+    private SpringBean(){
+        System.out.println("Spring Bean constructor");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("setBeanName");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("setApplicationContext");
+    }
+}
